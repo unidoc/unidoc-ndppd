9d1a68207ace78768ab3c7c6e7728e82990ab0a0
Alfred Hall <ahall@unidoc.io>
2026-08-28 22:59:38 +0000

    ci: install file (needed for the static-binary verify step)

--- torun.sh
+++ torun.sh
@@ -1,7 +1,6 @@
   git add -A
-  git commit -m "ci: add linux-headers (needed for linux/filter.h)"
+  git commit -m "ci: install file (needed for the static-binary verify step)"
   git push origin master
-
   git tag -d v1.0.0
   git push origin :refs/tags/v1.0.0
   git tag v1.0.0
