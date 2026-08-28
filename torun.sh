0891ae4b8a7deafd851f0d6d2899c1de60dc85e3
Alfred Hall <ahall@unidoc.io>
2026-08-28 23:06:08 +0000

    ci: build inside docker run instead of job-level container (JS actions don't run in Alpine arm64 containers)

--- torun.sh
+++ torun.sh
@@ -1,5 +1,5 @@
   git add -A
-  git commit -m "ci: install file (needed for the static-binary verify step)"
+  git commit -m "ci: build inside docker run instead of job-level container (JS actions don't run in Alpine arm64 containers)"
   git push origin master
   git tag -d v1.0.0
   git push origin :refs/tags/v1.0.0
